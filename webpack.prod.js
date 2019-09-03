75ac2905af54b04de3316933c941422d513eced3
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2019-09-03 21:07:05 +0300

    little tweak

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = merge(common, {
     mode: "production",
     output: {
-        filename: 'main[contentHash].js',
+        filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, "dist")
     },
     optimization: {
