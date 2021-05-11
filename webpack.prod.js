6d9da28a47ea8d2d796047cf877b72ad0cc80e71
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2021-05-11 16:11:31 +0300

    Update dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
@@ -13,7 +12,7 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist"),
   },
   optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+    minimizer: [new TerserPlugin()],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
